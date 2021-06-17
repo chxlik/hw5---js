1e913bdccde1d6a48de0f3c3d0d891ad8937f93b
chxlik <79369013+chxlik@users.noreply.github.com>
2021-06-17 19:01:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+// - Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому.
+//
+// 1) Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// a) отримує текст з параграфа з id "content"
+// console.log(document.getElementById("content"));
+
+// b) отримує текст з блоку з id "rules"
+// console.log(document.getElementById("rules"));
+
+// c) замініть текст параграфа з id 'content' на будь-який інший
+// document.getElementById("content").innerHTML = "Hello world"
+
+// d) замініть текст параграфа з id 'rules' на будь-який інший
+// document.getElementById('rules').innerHTML = 'mu name is Nikita'
+
+// e) змініть кожному елементу колір фону на червоний
+// let x = document.getElementById("content").style.backgroundColor = 'red'
+// let a = document.getElementById('rules').style.backgroundColor = 'red'
+
+// f) змініть кожному елементу колір тексту на синій
+
+// let f = document.getElementById('content').style.backgroundColor = 'blue'
+// let g = document.getElementById('rules').style.backgroundColor = 'blue'
+
+
+// g) отримати весь список класів елемента з id=rules і вивести їх в console.log
+//     let d = document.getElementById("rules")
+//     console.log(d.classList.value);
+
+// h) отримати всі елементи з класом fc_rules
+// let l = document.getElementsByClassName('fc_rules')
+// console.log(l)
+
+
+// i) поміняти колір тексту у всіх елементів fc_rules на червоний
+
+// let o = document.getElementsByClassName("fc_rules")
+// for(oo of o) {
+//     oo.style.backgroundColor = 'red'
+// }
+
+
